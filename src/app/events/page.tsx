5afdc1171b001c1590462b5bd2ff19b2fbc9f3fd
Eliad Drori <eliad.drori@gmail.com>
2026-02-12 20:55:32 +0200

    Update page.tsx

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -28,7 +28,7 @@ const events = [
   {
     id: 2,
     title: "יריד תחפושות לזכותה",
-    date: "2024-02-13",
+    date: "2025-02-13",
     time: "16:00",
     location: "שבט המושבה",
     description:
